821b3c91f21e2d56b87d6cc3aaa93b1803fba4b4
ako652 <akotabe0213@gmail.com>
2023-07-01 11:19:23 +0200

    deploy link

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -21,7 +21,7 @@ export default function Contact() {
           </div>
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt className="text-sm font-medium leading-6 text-gray-900">Email address</dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">akoatabe02@gmail.com</dd>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">akotabe02@gmail.com</dd>
           </div>
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt className="text-sm font-medium leading-6 text-gray-900">phone number</dt>
